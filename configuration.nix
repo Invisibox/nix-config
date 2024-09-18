3fe4bb784397f338e8f348fd7c5ba530e82221af
Invisibox <fortunateli@outlook.com>
2024-09-19 02:41:23 +0800

    add module asus-battery

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,7 @@
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
+  sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -74,6 +75,8 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+
+  services.pipewire.wireplumber.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
--- flake.nix
+++ flake.nix
@@ -2,18 +2,16 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS 官方软件源，这里使用 nixos-23.11 分支
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # 因此请将下面的 my-nixos 替换成你的主机名称
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # 这里导入之前我们使用的 configuration.nix，
-        # 这样旧的配置文件仍然能生效
         ./configuration.nix
+        nixos-hardware.nixosModules.asus-battery
       ];
     };
   };
