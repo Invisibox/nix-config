9332644b001fa5fc42ddb22ce016ed0cf9fb771f
Invisibox <fortunateli@outlook.com>
2025-07-28 02:27:59 +0800

    🔧 chore: 优化 mpv 配置并完善 home-manager 设置
    统一和迁移 mpv 配置，增强解码支持与 Wayland 兼容，丰富快捷键绑定，提高播放体验。
    新增 home-manager 备份扩展名配置，便于追踪 home.nix 变更历史。

--- flake.nix
+++ flake.nix
@@ -40,6 +40,10 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.zh = import ./home.nix;
+            
+            # 设置 home-manager 的备份文件扩展名
+            # 这样在修改 home.nix 时，home-manager 会自动备份旧的
+            home-manager.backupFileExtension = "bak";
 
             # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
             # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
