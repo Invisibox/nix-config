12121a6ee741a29fd04469a7e1e14996fe0c8a14
Invisibox <admin@djdog.cc>
2026-08-14 11:27:38 +0800

    ⬆️ chore(brave): update Origin Beta to 1.94.108

--- modules/apps/brave-origin/package.nix
+++ modules/apps/brave-origin/package.nix
@@ -3,7 +3,7 @@
   pkgs,
 }: let
   pname = "brave-origin-beta";
-  version = "1.94.107";
+  version = "1.94.108";
 
   deps = with pkgs; [
     alsa-lib
@@ -60,7 +60,7 @@ in
 
     src = pkgs.fetchurl {
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-origin-beta_${version}_amd64.deb";
-      hash = "sha256-pMgnV+1wl3xwnfmZj8muNSGog9lais+zHhBZ9tuLM9Y=";
+      hash = "sha256-IsL6xZ0Lw0ElfC2MEHT2LqNttaX7ZJEJ6h1OxRZ+aO8=";
     };
 
     dontConfigure = true;
