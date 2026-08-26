18ab126d97aa81cea1234f7aa6036dd4f9d64a12
Invisibox <admin@djdog.cc>
2026-08-27 00:37:56 +0800

    ✨ feat(display): add Shikane output profiles

--- home-manager/niri/default.nix
+++ home-manager/niri/default.nix
@@ -3,5 +3,7 @@
     config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/Documents/nix-config/home-manager/niri/config.kdl";
 
+  xdg.configFile."shikane/config.toml".source = ./shikane/config.toml;
+
   services.shikane.enable = true;
 }
