ae0b2ea243fc78b6a06483d44bad62465ff9951c
Invisibox <admin@djdog.cc>
2026-08-27 14:22:13 +0800

    ✨ feat(home): create XDG user directories

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,6 +19,12 @@
     ./zsh/zsh.nix
   ];
 
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    setSessionVariables = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
